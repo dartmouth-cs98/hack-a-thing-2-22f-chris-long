8e5d262973f08bbe2f4d57876f31582f2932a933
Chris Long <christopher.j.long.23@dartmouth.edu>
2022-09-28 19:30:01 -0400

    Partial progress towards threeJs personal site

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 import * as THREE from './node_modules/three/build/three.module.js';
 
+import { OrbitControls } from './node_modules/three/examples/jsm/controls/OrbitControls.js';
+
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -13,3 +15,94 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 camera.position.setZ(30);
 
 renderer.render(scene, camera);
+
+const geometry = new THREE.TorusGeometry(10, 3, 16, 100)
+const material = new THREE.MeshStandardMaterial( { color: 0xFF6347 } );
+
+const torus = new THREE.Mesh( geometry, material ); 
+
+scene.add(torus)
+
+const pointLight = new THREE.PointLight(0xffffff)
+pointLight.position.set(5,5,5)
+
+scene.add(pointLight)
+
+const ambientLight = new THREE.AmbientLight(0xffffff)
+scene.add(ambientLight)
+
+const lightHelper = new THREE.PointLightHelper(pointLight)
+scene.add(lightHelper)
+
+const gridHelper = new THREE.GridHelper(200, 50)
+scene.add(gridHelper)
+
+const controls = new OrbitControls(camera, renderer.domElement);
+
+function addStar() {
+    const geometry = new THREE.SphereGeometry(0.25, 24, 24);
+    const material = new THREE.MeshStandardMaterial( { color: 0xffffff })
+    const star = new THREE.Mesh( geometry, material );
+
+    const [x,y,z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread( 100 ));
+
+    star.position.set(x,y,z);
+    scene.add(star)
+}
+
+Array(200).fill().forEach(addStar)
+
+const spaceTexture = new THREE.TextureLoader().load('./img/spaceGeneric.jpeg')
+scene.background = spaceTexture;
+
+const chrisTexture = new THREE.TextureLoader().load('./img/ProfilePhoto.png')
+
+const chris = new THREE.Mesh(
+    new THREE.BoxGeometry(3,3,3),
+    new THREE.MeshBasicMaterial({map: chrisTexture})
+)
+
+scene.add(chris)
+
+const ballTexture = new THREE.TextureLoader().load('./img/basketball.jpeg');
+
+const ball = new THREE.Mesh(
+    new THREE.SphereGeometry(3,32,32),
+    new THREE.MeshStandardMaterial({map: ballTexture})
+)
+
+scene.add(ball)
+
+ball.position.z = 15;
+
+function moveCamera() {
+    const t = document.body.getBoundingClientRect().top;
+    
+    ball.rotation.x += 0.05;
+    ball.rotation.y += 0.075;
+    ball.rotation.z += 0.05;
+
+    chris.rotation.y += 0.01;
+    chris.rotation.z += 0.01;
+
+    camera.position.x = t * -0.0002;
+    camera.position.y = t * -0.0002;
+    camera.position.z = t * -0.1;
+
+}
+
+document.body.onscroll = moveCamera
+
+function animate() {
+    requestAnimationFrame( animate );
+
+    torus.rotation.x += 0.01;
+    torus.rotation.y += 0.005;
+    torus.rotation.z += 0.01;
+
+    controls.update();
+
+    renderer.render(scene, camera);
+}
+
+animate()
