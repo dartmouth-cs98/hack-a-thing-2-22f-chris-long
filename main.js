09f56087b43a40936d4268256d570e237229f90a
Chris Long <christopher.j.long.23@dartmouth.edu>
2022-09-30 13:09:37 -0400

    Trying to fix bug again

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-import * as THREE from '/node_modules/three/build/three.module.js';
+import * as THREE from 'three';
 
-import * as POSTPROCESSING from '/node_modules/postprocessing/build/postprocessing.mjs';
+import * as POSTPROCESSING from 'postprocessing';
 
 /*
 import { OrbitControls } from './node_modules/three/examples/jsm/controls/OrbitControls.js';
